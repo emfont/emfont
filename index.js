124b2667e76994b37088688dee431de654a4f0dd
Edit-Mr <elvismao.070512@gmail.com>
2024-06-15 15:37:20 +0800

    v0.2 finished

--- index.js
+++ index.js
@@ -1,164 +1,73 @@
-/** @format */
+const emfont = (function() {
+    // Function to get all elements with class starting with .emfont and load the custom font
+    function loadCustomFonts(callback) {
+        const elements = document.querySelectorAll("[class^='emfont']");
+        let fonts = {};
+        let promises = [];
 
-const express = require("express");
-const fs = require("fs");
-const Fontmin = require("fontmin");
-const path = require("path");
-const app = express();
-const port = 3000;
-
-// Middleware to parse JSON body
-app.use(express.json());
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type, Authorization, words"
-    );
-    next();
-});
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/list", (req, res) => {
-    const fonts = require("./Database/fonts.json");
-    res.json(fonts);
-});
-
-// Route to handle font downloads
-app.get("/f/:fileName/:fontName", (req, res) => {
-    const { fileName, fontName } = req.params;
-    logAccess(fontName, "", req, "download");
-    res.download(
-        path.join(__dirname, "fonts", "generated", fileName, fontName)
-    );
-});
-
-// Route to handle font generate
-app.post("/g/:font", async (req, res) => {
-    try {
-        const words = req.body.words;
-        if (!words) {
-            return res.status(400).send("Words are required");
-        }
-        console.log(words);
-        var fontID = req.params.font;
-
-        let generated = require("./Database/generated.json");
-        if (generated[words] && generated[words][fontID]) {
-            const fontData = require("./Database/fonts.json")[fontID];
-            logAccess(fontData.name, words, req);
-            return res.json({
-                url: `https://font.emtech.cc/f/${generated[words][fontID]}/${fontData.output}`,
-                font: fontData.name,
-                style: fontData.style,
-                weight: fontData.weight,
-            });
-        }
-
-        // load Database/font.json
-        const fontList = require("./Database/fonts.json");
-        // check if font is in the list
-        if (!fontList[fontID]) {
-            return res.status(400).send("Font not found");
-        }
-
-        const fontData = fontList[fontID];
-        const fontFile = fontData.file;
-        const fontName = fontData.name;
-        // Check if words are provided
-
-        // generate random file name
-        let outputID = generateID(10);
-        console.log(outputID);
-        while (
-            fs.existsSync(path.join(__dirname, "fonts", "generated", outputID))
-        ) {
-            outputID = generateID();
-        }
-
-        // Generate font file
-        await generateFont(fontFile, words, outputID);
-
-        generated = require("./Database/generated.json");
-        if (!generated[words]) {
-            generated[words] = {};
-        }
-        generated[words][fontID] = outputID;
-        fs.writeFileSync(
-            path.join(__dirname, "Database", "generated.json"),
-            JSON.stringify(generated)
-        );
-
-        // Log access
-        logAccess(fontName, words, req);
-        res.json({
-            url: `https://font.emtech.cc/f/${outputID}/${fontData.output}`,
-            font: fontName,
-            style: fontData.style,
-            weight: fontData.weight,
-        });
-    } catch (error) {
-        console.error("Error:", error.message);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-// Function to generate font file with specified words
-async function generateFont(originalFontPath, words, fileName) {
-    const fontmin = new Fontmin()
-        .src(path.join(__dirname, "fonts", "original", originalFontPath))
-        .use(
-            Fontmin.glyph({
-                text: words,
-                hinting: false, // keep ttf hint info (fpgm, prep, cvt). default = true
-            })
-        )
-        .use(
-            Fontmin.ttf2woff({
-                deflate: true, // deflate woff. default = false
-            })
-        )
-        .dest(path.join(__dirname, "fonts", "generated", fileName));
-
-    return new Promise((resolve, reject) => {
-        fontmin.run(function (err, files) {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(files);
+        elements.forEach(element => {
+            // Get target font name from element class
+            const fontName = element.className
+                .split(" ")
+                .find(name => name.startsWith("emfont-"))
+                .replace("emfont-", "");
+            const words = element.textContent.trim(); // Custom words from element text
+            if (fontName && words) {
+                if (!fonts[fontName]) {
+                    fonts[fontName] = words;
+                } else {
+                    fonts[fontName] += words;
+                }
             }
         });
-    });
-}
 
-// Function to log access
-function logAccess(fontName, words, req, action = "generate") {
-    const logEntry = {
-        font: fontName,
-        words,
-        timestamp: new Date().toISOString(),
-        userAgent: req.headers["user-agent"],
-        ip: req.ip,
+        // Load custom fonts
+        for (const fontName in fonts) {
+            var textList = fonts[fontName];
+            // Remove duplicate characters and sort by UTF encoding
+            textList = Array.from(new Set(textList.split("")))
+                .sort()
+                .join("");
+            
+            let promise = fetch("https://font.emtech.cc/g/" + fontName, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ words: textList }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                const font = new FontFace(data.font, `url(${data.url})`, {
+                    style: data.style,
+                    weight: data.weight,
+                });
+                // Add to the document.fonts (FontFaceSet)
+                return font.load().then(loadedFont => {
+                    document.fonts.add(loadedFont);
+                    // Apply the font to the elements
+                    elements.forEach(element => {
+                        if (element.className.includes(`emfont-${fontName}`)) {
+                            element.style.fontFamily = data.font;
+                        }
+                    });
+                });
+            });
+            
+            promises.push(promise);
+        }
+
+        // Wait for all fonts to load
+        Promise.all(promises).then(() => {
+            if (callback && typeof callback === 'function') {
+                callback();
+            }
+        });
+    }
+
+    return {
+        init: function(callback) {
+            loadCustomFonts(callback);
+        }
     };
-    fs.appendFileSync(action + ".log", JSON.stringify(logEntry) + "\n");
-}
-
-//: 10 random characters and numbers
-const generateID = length => {
-    const characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    return Array.from({ length }, () =>
-        characters.charAt(Math.floor(Math.random() * characters.length))
-    ).join("");
-};
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+})();
--- public/emfont.js
+++ public/emfont.js
@@ -1,5 +1,7 @@
- // Function to get all elements with class starts with .emfont and load the custom font
- function loadCustomFonts() {
+/** @format */
+
+// Function to get all elements with class starts with .emfont and load the custom font
+function loadCustomFonts() {
     const elements = document.querySelectorAll("[class^='emfont']");
     let fonts = {};
     elements.forEach(element => {
@@ -17,36 +19,30 @@
     for (const fontName in fonts) {
         var textList = fonts[fontName];
         // 移除重複字體，並按照 UTF 編碼排序
-        textList = Array.from(new Set(textList.split(""))).sort().join("");
+        textList = Array.from(new Set(textList.split("")))
+            .sort()
+            .join("");
         fetch("https://font.emtech.cc/g/" + fontName, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ words:textList }),
+            body: JSON.stringify({ words: textList }),
         })
             .then(response => response.json())
             .then(data => {
-                console.warn(
-                    "emfont: 字體 " +
-                        data.font +
-                        "已生成完成，若網頁沒有變動文字可以直接直接載入網址\n" +
-                        data.url +
-                        " 來使用。"
-                );
-                const font = new FontFace(
-                    data.font,
-                    `url(${data.url})`,
-                    {
-                        style: data.style,
-                        weight: data.weight,
-                    }
-                );
-
+                const font = new FontFace(data.font, `url(${data.url})`, {
+                    style: data.style,
+                    weight: data.weight,
+                });
                 // Add to the document.fonts (FontFaceSet)
                 document.fonts.add(font);
+                // Load the font
+                font.load().then(loadedFont => {
+                    document.fonts.add(loadedFont);
+                    // Apply the font to the element
+                    element.style.fontFamily = data.font;
+                });
             });
     }
 }
-// Call the function to load custom fonts when the document is ready
-document.addEventListener("DOMContentLoaded", loadCustomFonts);
