7ad08cf5fd7504d7b1b1b1398a92e3b7fb23e5fa
Elvis Mao <info@elvismao.com>
2024-07-05 02:05:35 +0000

    ESlint config ignore emfont

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+      rules: {
+        'no-unused-vars': ['error', { varsIgnorePattern: 'emfont' }]
+      }
+    }
+  },
   pluginJs.configs.recommended,
 ];
