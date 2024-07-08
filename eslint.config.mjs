c2283973a83736bb6abf5554246e82cabefbfd3c
Elvis Mao <info@elvismao.com>
2024-07-09 00:36:34 +0800

    use import instead of require

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
+/** @format */
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  {
-    languageOptions: {
-      globals: globals.browser
+    {
+        languageOptions: {
+            globals: globals.browser,
+        },
+        rules: {
+            "no-unused-vars": ["error", { varsIgnorePattern: "emfont" }],
+        }
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: 'emfont' }]
-    }
-  },
-  pluginJs.configs.recommended,
+    pluginJs.configs.recommended,
 ];
--- src/workflows/csscomb.js
+++ src/workflows/csscomb.js
@@ -1,3 +1,3 @@
-var Comb = require('csscomb');
+import Comb from "csscomb";
 var comb = new Comb('zen');
 comb.processPath('src/static/css');
