ffd603c3df9f8e549180dfe9363005eee3c04c3e
Edit-Mr <elvismao.070512@gmail.com>
2024-07-08 22:25:59 +0800

    Add marque, and fix bug for not setting emfont as first class

--- public/emfont.js
+++ public/emfont.js
@@ -3,24 +3,24 @@
 const emfont = (function () {
     // Function to get all elements with class starting with .emfont and load the custom font
     function loadCustomFonts(callback) {
-        const elements = document.querySelectorAll("[class^='emfont']");
+        const elements = document.querySelectorAll("[class*='emfont']");
         let fonts = {};
         let promises = [];
         elements.forEach(element => {
-            // Get target font name from element class
-            const fontName = element.className
+            // Get all font names from element class
+            const fontNames = element.className
                 .split(" ")
-                .find(name => name.startsWith("emfont-"))
-                .replace("emfont-", "");
+                .filter(name => name.startsWith("emfont-"))
+                .map(name => name.replace("emfont-", ""));
             const words = element.textContent.trim(); // Custom words from element text
-            if (fontName && words) {
-                if (!fonts[fontName]) {
-                    fonts[fontName] = words;
-                } else {
-                    fonts[fontName] += words;
-                }
-            }
+
+            fontNames.forEach(fontName => {
+                if (fontName && words)
+                    fonts[fontName] =
+                        (fonts[fontName] ? fonts[fontName] : "") + words;
+            });
         });
+        const cssElement = document.createElement("style");
 
         // Load custom fonts
         for (const fontName in fonts) {
@@ -29,7 +29,6 @@ const emfont = (function () {
             textList = Array.from(new Set(textList.split("")))
                 .sort()
                 .join("");
-
             let promise = fetch("https://font.emtech.cc/g/" + fontName, {
                 method: "POST",
                 headers: {
@@ -39,23 +38,17 @@ const emfont = (function () {
             })
                 .then(response => response.json())
                 .then(data => {
-                    const fontCSSName =  data.font.zh;
+                    const fontCSSName = data.font.zh;
                     const font = new FontFace(fontCSSName, `url(${data.url})`);
+                    cssElement.innerHTML += `.emfont-${fontName} { font-family: '${fontCSSName}', sans-serif; }`;
                     // Add to the document.fonts (FontFaceSet)
                     return font.load().then(loadedFont => {
                         document.fonts.add(loadedFont);
-                        // Apply the font to the elements
-                        elements.forEach(element => {
-                            if (
-                                element.className.includes(`emfont-${fontName}`)
-                            ) {
-                                element.style.fontFamily = fontCSSName;
-                            }
-                        });
                     });
                 });
             promises.push(promise);
         }
+        document.head.appendChild(cssElement);
 
         // Wait for all fonts to load
         Promise.all(promises).then(() => {
