f38f85d96af9d9fb669f6a602da6b88baea20052
Edit-Mr <elvismao.070512@gmail.com>
2024-07-24 12:06:02 +0800

    Update nav menu

--- src/front.js
+++ src/front.js
@@ -28,7 +28,7 @@ app.get("/:view", (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
