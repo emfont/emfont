f3d4efae0ba2b44559ecd3fefc5694248c9b75b2
Edit-Mr <elvismao.070512@gmail.com>
2024-07-25 16:59:18 +0800

    fix eslint with a lot of research

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-    {
-        languageOptions: {
-            globals: globals.browser,
-        },
-        rules: {
-            "no-unused-vars": ["error", { varsIgnorePattern: "emfont" }],
-        }
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    pluginJs.configs.recommended,
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "emfont" }],
+    }
+  },
+  pluginJs.configs.recommended,
 ];
