2dd582911914d9a6e79507411619595653a89122
Elvis Mao <info@elvismao.com>
2024-08-01 15:21:51 +0800

    page transition

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,19 +1,33 @@
 /** @format */
 
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => {
     document.querySelector(".fader").classList.add("fade-in");
-    console.log("hohoho")
+};
 
-    document.querySelector("#copy").addEventListener("click", () => {
-        navigator.clipboard.writeText(code.innerText).then(() => {
-            document
-                .querySelector(".code")
-                .style.setProperty("--copied-bg", "#e5ffe4");
+document.addEventListener("DOMContentLoaded", () => {
+    // get scroll position from session
+    if (
+        getComputedStyle(document.querySelector(":root")).getPropertyValue(
+            "--dashboard"
+        )
+    ) {
+        const scroll = sessionStorage.getItem("scroll");
+        if (scroll) {
+            window.scrollTo(0, scroll);
+            sessionStorage.removeItem("scroll");
+        }
+    }
+
+    if (document.querySelector("#copy"))
+        document.querySelector("#copy").addEventListener("click", () => {
+            navigator.clipboard.writeText(code.innerText).then(() => {
+                document
+                    .querySelector(".code")
+                    .style.setProperty("--copied-bg", "#e5ffe4");
+            });
         });
-    });
 
     const aElements = document.querySelectorAll("a");
-
     aElements.forEach(link => {
         link.addEventListener("click", e => {
             e.preventDefault();
@@ -23,8 +37,16 @@ document.addEventListener("DOMContentLoaded", () => {
                 // if new tab
                 if (link.getAttribute("target") === "_blank")
                     window.open(href, "_blank");
-                else window.location.href = href;
-            }, 400);
+                else {
+                    if (
+                        getComputedStyle(
+                            document.querySelector(":root")
+                        ).getPropertyValue("--dashboard")
+                    )
+                        sessionStorage.setItem("scroll", window.scrollY);
+                    window.location.href = href;
+                }
+            }, 100);
         });
     });
 });
